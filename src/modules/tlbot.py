98ecc6c84e143f5db0f81cb9db3e640f8ee9ea3b
Fedir Katushonok <46378315+CeSiumUA@users.noreply.github.com>
2024-01-02 08:50:59 +0200

    Added device stats

--- src/modules/tlbot.py
+++ src/modules/tlbot.py
@@ -14,6 +14,7 @@ class TlBot(TelegramMessageSenderInterface):
     YOUTUBE_DOWNLOAD = 0
     POWER_STATE_SELECTOR = 1
     POWER_STATE_AFTER_SELECT = 2
+    STATS_AFTER_DEVICE_SELECT = 3
 
     def __init__(self, publisher: MqttPublisherInterface, device_manager: DeviceManagerInterface, logger: logging.Logger):
         self.__token = Env.get_tl_token()
@@ -95,8 +96,11 @@ class TlBot(TelegramMessageSenderInterface):
             keyboard[0].append(InlineKeyboardButton("Disable forced state", callback_data=json_data_no_forced))
 
         reply_markup = InlineKeyboardMarkup(keyboard)
+
+        current_state = 'On' if stored_device[MongoDbAccess.DEVICE_POWER_ON_FIELD] else 'Off'
+
         await query.edit_message_text(
-            text=f"Choose a state for {device_name}", reply_markup=reply_markup
+            text=f"Choose a state for {device_name}. Current state: {current_state}", reply_markup=reply_markup
         )
         return self.POWER_STATE_AFTER_SELECT
     
@@ -120,6 +124,57 @@ class TlBot(TelegramMessageSenderInterface):
 
         return ConversationHandler.END
 
+    async def __stats_device_selector(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        self.__logger.info('got device stats select request')
+
+        keyboard = [[]]
+        with MongoDbAccess() as mongo_client:
+            for device in mongo_client.get_devices_names():
+                device_name = device[mongo_client.DEVICE_NAME_FIELD]
+                keyboard[0].append(InlineKeyboardButton(device_name, callback_data=device_name))
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text(
+            text="Choose device", reply_markup=reply_markup
+        )
+
+        return self.STATS_AFTER_DEVICE_SELECT
+
+    async def __stats_after_device_select(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        self.__logger.info('got stats after device select request')
+        
+        query = update.callback_query
+        await query.answer()
+
+        device_name = query.data
+
+        with MongoDbAccess() as mongo_client:
+            stored_device = mongo_client.get_device_by_name(device_name)
+
+        reply_text = 'Device stats:\n'
+        if MongoDbAccess.DEVICE_NAME_FIELD in stored_device:
+            reply_text += f'Device name: {stored_device[MongoDbAccess.DEVICE_NAME_FIELD]}\n'
+        if MongoDbAccess.DEVICE_IS_DARK_FIELD in stored_device:
+            is_dark = 'Yes' if stored_device[MongoDbAccess.DEVICE_IS_DARK_FIELD] else 'No'
+            reply_text += f'Is device in dark: {is_dark}\n'
+        if MongoDbAccess.DEVICE_IS_DEVICE_SLEEP in stored_device:
+            is_sleep = 'Yes' if stored_device[MongoDbAccess.DEVICE_IS_DEVICE_SLEEP] else 'No'
+            reply_text += f'Is device sleep: {is_sleep}\n'
+        if MongoDbAccess.DEVICE_TYPE_FIELD in stored_device:
+            reply_text += f'Device type: {MongoDbAccess.DEVICE_TYPE_FIELD}\n'
+        if MongoDbAccess.DEVICE_TEMPERATURE_FIELD in stored_device:
+            reply_text += f'Device temperature: {MongoDbAccess.DEVICE_TEMPERATURE_FIELD}\n'
+        if MongoDbAccess.DEVICE_IS_POWER_FORCED_FIELD in stored_device:
+            if stored_device[MongoDbAccess.DEVICE_IS_POWER_FORCED_FIELD]:
+                reply_text += 'Device is in power forced state\n'
+            else:
+                reply_text += 'Device is not power-forced\n'
+        if MongoDbAccess.DEVICE_TOTAL_ENERGY_FIELD in stored_device:
+            reply_text += f'Device total energy: {stored_device[MongoDbAccess.DEVICE_TOTAL_ENERGY_FIELD]}\n'
+
+        await query.edit_message_text(reply_text)
+        return ConversationHandler.END
+
     async def __cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         return ConversationHandler.END
     
@@ -138,12 +193,14 @@ class TlBot(TelegramMessageSenderInterface):
             entry_points=[
                 CommandHandler("ytdownload", self.__video_download_start, filters=filters.Chat(self.__chat_id)),
                 CommandHandler("ipaddress", self.__get_ip_address, filters=filters.Chat(self.__chat_id)),
-                CommandHandler("power", self.__power_the_device, filters=filters.Chat(self.__chat_id))
+                CommandHandler("power", self.__power_the_device, filters=filters.Chat(self.__chat_id)),
+                CommandHandler("stats", self.__stats_device_selector, filters=filters.Chat(self.__chat_id))
             ],
             states={
                 self.YOUTUBE_DOWNLOAD: [MessageHandler(filters=filters.TEXT, callback=self.__video_download)],
                 self.POWER_STATE_SELECTOR: [CallbackQueryHandler(self.__power_select_device_state)],
-                self.POWER_STATE_AFTER_SELECT: [CallbackQueryHandler(self.__power_after_state_select)]
+                self.POWER_STATE_AFTER_SELECT: [CallbackQueryHandler(self.__power_after_state_select)],
+                self.STATS_AFTER_DEVICE_SELECT: [CallbackQueryHandler(self.__stats_after_device_select)]
             },
             fallbacks=[CommandHandler("cancel", self.__cancel)]
         )
